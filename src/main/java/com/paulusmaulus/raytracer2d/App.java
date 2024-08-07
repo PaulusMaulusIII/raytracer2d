3b10f9a19fd9480605a279c7e74b3e24349c06d4
Paul Schröder <paulschroeder05@outlook.de>
2024-08-07 15:48:43 +0200

    Initial commit

---
+++ src/main/java/com/paulusmaulus/raytracer2d/App.java
@@ -0,0 +1,30 @@
+package com.paulusmaulus.raytracer2d;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame implements Runnable {
+
+    public App() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        app.setSize(720, 720);
+        app.setTitle("2D Ray-Tracing game engine");
+        app.setVisible(true);
+        app.run();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            repaint();
+        }
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.paulusmaulus.raytracer2d {
+    requires java.desktop;
+
+    exports com.paulusmaulus.raytracer2d;
+}
